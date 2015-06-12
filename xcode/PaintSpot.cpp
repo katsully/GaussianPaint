1f2d798d17bc5e5a2e45545d370e357adf3170e7
Kat Sullivan <kmsullivan012@gmail.com>
2015-06-12 12:17:16 -0400

    really working

--- xcode/PaintSpot.cpp
+++ xcode/PaintSpot.cpp
@@ -18,44 +18,21 @@ PaintSpot::PaintSpot(){
 PaintSpot::PaintSpot( Vec2f mMousePosition ){
     mMouseLoc = mMousePosition;
     mNOff = Vec2f ( randFloat( 1000 ), randFloat( 1000 ) );
-    mColor = Color ( CM_HSV, .5f, 1, 1  );
+    mColor = Color ( CM_HSV, randFloat(), 1, 1  );
     noiseCount = randFloat( 1000.0f );
     mPerlin = Perlin();
 }
 
 void PaintSpot::createVectors(){
     mVectors.clear();
-    float x;
-    float y;
-    for( int i = 0; i < 20; i++ ){
-        if( i < 5 ){
-            x = lmap( mPerlin.fBm(mNOff.x), -1.0f, 1.0f, mMouseLoc.x, mMouseLoc.x + 75.0f );
-            y = lmap( mPerlin.fBm(mNOff.y), -1.0f, 1.0f, mMouseLoc.y, mMouseLoc.y + 75.0f );
-        } else if (i >= 5 && i < 10 ){
-            x = lmap( mPerlin.fBm(mNOff.x), -1.0f, 1.0f, mMouseLoc.x, mMouseLoc.x + 75.0f );
-            y = lmap( mPerlin.fBm(mNOff.y), -1.0f, 1.0f, 0.0f, abs( mMouseLoc.y - 75.0f ) );
-        } else if ( i >= 10 && i < 15 ){
-            x = lmap( mPerlin.fBm(mNOff.x), -1.0f, 1.0f, 0.0f, abs( mMouseLoc.x - 75.0f ) );
-            y = lmap( mPerlin.fBm(mNOff.y), -1.0f, 1.0f, 0.0f, abs( mMouseLoc.y - 75.0f ) );
-        } else {
-            x = lmap( mPerlin.fBm(mNOff.x), -1.0f, 1.0f, 0.0f, abs( mMouseLoc.x - 75.0f ) );
-            y = lmap( mPerlin.fBm(mNOff.y), -1.0f, 1.0f, mMouseLoc.y, mMouseLoc.y + 75.0f );
-        }
-        mVectors.push_back( Vec2f( x, y ) );
-        mNOff += Vec2f( 5.0f, 5.0f );
+    Vec2f newPoint;
+    for(int i = 0; i < 100; i++){
+        newPoint.x = lmap( mPerlin.fBm(mNOff.x), -0.5f, 0.5f, -200.0f, 200.0f );
+        newPoint.y = lmap( mPerlin.fBm(mNOff.y), -0.5f, 0.5f, -200.0f, 200.0f );
+        mVectors.push_back( newPoint + mMouseLoc);
+        mNOff += Vec2f( 0.01, 0.01 );
+
     }
-//    int radius = 100;
-//    float numPoints = 100.0f;
-//    float pointDegree = 300 / numPoints;
-//    for( int i=0; i < numPoints; i++ ){
-//        float x = cos( pointDegree * i) * radius;
-//        float y = sin( pointDegree * i) * radius;
-//        float n = mPerlin.noise( noiseCount ) * 20;
-//        mVectors.push_back( Vec2f( x + n + mMouseLoc.x, y + n + mMouseLoc.y ) );
-//        noiseCount += 0.01f;
-//    }
-
-
 }
 
 void PaintSpot::render(){
